4956703bc40883600ea2921548eca38cb054d9e4
andreasnarep <andreasnarep2@gmail.com>
2024-05-01 12:32:02 +0300

    Firebase init.

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    //baseURL: 'https://core-memento-421408.lm.r.appspot.com',
-    baseURL: 'http://192.168.1.234:8080',
+    baseURL: 'https://inventory-demo-422008.lm.r.appspot.com',
+    //baseURL: 'http://192.168.1.234:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
